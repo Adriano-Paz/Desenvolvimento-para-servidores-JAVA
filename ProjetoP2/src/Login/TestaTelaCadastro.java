aaea76e644745fc83e862b102f695da11cef1c25
Adriano-Paz <adriano.agp15@gmail.com>
2025-06-09 19:49:38 -0300

    Reapply "Este arquivo é referente a prova2 de desenvolvimento-para-servidores"
    This reverts commit 3e4cd653cf3100911998b2196602dbef6a5291e4.

---
+++ ProjetoP2/src/Login/TestaTelaCadastro.java
@@ -0,0 +1,17 @@
+package Login;
+
+/**
+ *
+ * @author adria
+ */
+public class TestaTelaCadastro {
+
+    public static void main(String[] args) {
+        // Instância da classe TelaCadastro
+        TelaCadastro t = new TelaCadastro();
+
+        //Método de chamada do método construtor e abertura de tela
+        t.abreTela();
+    }
+
+}
